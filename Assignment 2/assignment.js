fc161b0f50f6d3d08cc478bdebf6e4b9e7797242
hsachan13 <68600204+hsachan13@users.noreply.github.com>
2021-03-11 23:56:10 +0530

    Update assignment.js

--- Assignment 2/assignment.js
+++ Assignment 2/assignment.js
@@ -1,6 +1,4 @@
-var Promise = require('bluebird');
 
-/*Promise.delay(3000,'First').then(())*/
 const mongoose = require('mongoose');
 
 mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true, useUnifiedTopology: true });
