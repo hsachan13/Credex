9136cb4b495ff48bcb4945bd415e977cf4931262
hsachan13 <68600204+hsachan13@users.noreply.github.com>
2021-03-12 15:05:26 +0530

    Update library.js

--- Assignment 2/library.js
+++ Assignment 2/library.js
@@ -1,7 +1,15 @@
 
+module.exports = {
+    connection: function () {
+        const mongoose = require('mongoose');
 
-const ipl = {
-    teams: 8,
-    winner: "Mumbai Indians"
+        mongoose.connect("mongodb://localhost:27017/credex", { useNewUrlParser: true, useUnifiedTopology: true })
+            .then(() => console.log("mongodb connected"))
+            .catch((err) => console.log(err));
+    }
 }
-module.exports = ipl;
+// const ipl = {
+//     teams: 8,
+//     winner: "Mumbai Indians"
+// }
+// module.exports = ipl;
