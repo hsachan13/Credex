fce125ab31ec4bb43baa0947656bc9969554524a
hsachan13 <68600204+hsachan13@users.noreply.github.com>
2021-03-14 22:33:58 +0530

    Add files via upload

---
+++ Assignment 3/models/students.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const studentSch = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+            type: String,
+            required: true,
+            unique: [true, "Emailid already taken"],
+            validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid Email")
+            }
+        }
+    },
+        phone: {
+            type: Number,
+            min: 10,
+            required:true
+    },
+    address: {
+        type: String,
+        required:true
+    }
+    
+})
+
+
+//collections
+const Student = new mongoose.model('Student', studentSch);
+module.exports = Student;
